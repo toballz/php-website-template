d29ff3908027a68d9b3c71aab0e652dbbb75bd6a
toballz <42551340+toballz@users.noreply.github.com>
2024-09-30 02:11:05 -0400

    Update coff.php

--- coff.php
+++ coff.php
@@ -3,7 +3,7 @@ class db{
     const servername = "localhost"; 
     const username = "root";//u723978224_wwwwwwwww
     const password = "";//wwwwwwwwwww
-    const dbname = "continuefrom";//u723978224_eeeeeeeeee
+    const dbname = "concona";//u723978224_eeeeeeeeee
     // Create a connection
     static function conn(){
         $rr= new mysqli(self::servername, self::username, self::password, self::dbname);
@@ -12,10 +12,19 @@ class db{
         }else{return $rr;}
     }
     static function stmt($stm){
-        return mysqli_query(self::conn(), $stm);
+        try{
+            return mysqli_query(self::conn(), $stm);
+        }catch(Exception $e){ 
+            error_log($e->getMessage());
+            return false;
+        }
+    }
+
+    static function real_escape_string($v){
+       return  mysqli_real_escape_string(self::conn(),$v);
     }
 }
 const __dir=__dir__;
 //donot delete edit as needed
-include 'aapp/e39e22bb8b4e0f238180959c70eaf4c7.php';
+//include 'aapp/e39e22bb8b4e0f238180959c70eaf4c7.php';
 ?>
